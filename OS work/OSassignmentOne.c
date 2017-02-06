2dd37617181777a6f415d177adc58bd83997574f
draw7redlines <vetromark@gmail.com>
2017-02-06 03:34:59 -0500

    Output and sorting algorithms. It's a little bugged  since when new processes are taken they have a decrement by one, and I can't seem to
    fix it yet. it's 3:30 in the morning though, so I'm going to bed and
    gonna work on this later

--- OS work/OSassignmentOne.c
+++ OS work/OSassignmentOne.c
@@ -169,8 +169,7 @@ void roundRobin(char **keyWord)
     temp = (struct node *) malloc(sizeof(struct node));
 
 
-    //testing to see if we populated it correctly
-    //delete this later or this will never work!!!
+
     printf("===========here's the contents of the linked list==========");
     temp = root;
     while(temp!=NULL)
@@ -182,7 +181,222 @@ void roundRobin(char **keyWord)
         temp=temp->next;
     }
 
+
+
+    temp=root;
+    sortListByArrivalTime(temp);
+    root=temp;
+
+    printf("===========here's the contents of the linked list after a sort==========");
+    while(temp!=NULL)
+    {
+        printf("\n");
+        printf("process name: %s \n", temp->processNumber);
+        printf("arrival time: %d \n", temp->arrivalTime);
+        printf("burst time: %d \n", temp->burstTime);
+        temp=temp->next;
+    }
+
+
+    //start of the actual processing step
+
+    listNode *systemsTracker;
+    systemsTracker = (struct node *) malloc(sizeof(struct node));
+
+    int time=0;
+    int idleCheck = 0;
+    int gameOver=0;
+    int currentProc = 0;
+    int maxProcNum = 0;
+    int jumpProcFlag = 0;
+    //int minProcNum = 0;
+
+    while(time<=runTime && gameOver==0)
+    {
+        //termination case set
+        gameOver=1;
+        jumpProcFlag = 0;
+
+        //process arrives, which oversteps standard quantum rules and
+        //makes a report no matter what
+        temp=root;
+        for(int i=0; i<processCount; i++)
+        {
+            if(temp->arrivalTime==time)
+            {
+                printf("Time %d: %s arrived\n", time, temp->processNumber);
+                idleCheck++;
+                maxProcNum++;
+                jumpProcFlag =1;
+
+            }
+            temp=temp->next;
+        }
+
+        if(jumpProcFlag==1)
+            currentProc++;
+
+
+        //process finishes, which oversteps quantum rules and makes
+        //a report no matter what
+        temp=root;
+        for(int i=0; i<processCount; i++)
+        {
+            if(temp->burstTime==0)
+            {
+                printf("Time %d: %s finished\n", time, temp->processNumber);
+                idleCheck--;
+                maxProcNum--;
+                temp->burstTime=-1;
+            }
+            temp=temp->next;
+        }
+
+
+
+        //pick our poison (process)
+        //find currentProc instance of node without -1 burst. currentProc increments until it hits
+        //max process which is highest node it should go up to. Then it resets to the
+        //first instance of a node without a -1 burst, then increments up to the next one
+        // without exceeding max process number
+        temp=root;
+        int instance = 1;
+        for(int i=0; i<processCount; i++)
+        {
+            if(instance<currentProc&&temp->burstTime!=-1)
+            {
+                temp=temp->next;
+                instance++;
+            }
+
+            else if(instance<currentProc)
+            {
+                temp=temp->next;
+            }
+        }
+
+
+
+
+
+
+        //systems report on current operations, this is for all the marbles
+        if(time%quantum==0)
+        {
+            //it is idling
+            if(idleCheck==0)
+            {
+                printf("Time %d: IDLE\n", time);
+            }
+
+            //we've got something selected, and we report it's remaining burst
+            else
+            {
+                printf("Time %d: %s selected (burst %d) \n", time, temp->processNumber, temp->burstTime);
+            }
+
+
+        }
+
+
+        if(idleCheck>0)
+            temp->burstTime--;
+
+
+
+        //shift the process if we're on a quantum
+        if(time%quantum==0)
+        currentProc++;
+
+
+        if(currentProc>maxProcNum)
+        {
+            if(idleCheck==0)
+                currentProc=0;
+            else
+                currentProc=1;
+        }
+
+
+        //printf("current process set to %d \n", currentProc);
+
+
+
+
+        //we're done
+        temp=root;
+        for(int i=0; i<processCount; i++)
+        {
+            if(temp->burstTime!=-1)
+                gameOver=0;
+
+            temp=temp->next;
+        }
+
+        time++;
+
+    }
+
+
+
 }
+
+
+
+void sortListByArrivalTime(struct node *temp)
+{
+    int switchFlag, i;
+    struct node *helper= NULL;
+    struct node *helperTwo = NULL;
+    struct node *root = temp;
+
+    helper = (struct node *) malloc(sizeof(struct node));
+
+    if(root==NULL)
+    {
+        return;
+    }
+
+    switchFlag=1;
+
+    while(switchFlag!=0)
+    {
+        switchFlag=0;
+        root=temp;
+        while(root->next!=NULL)
+        {
+
+            if(root->arrivalTime>root->next->arrivalTime)
+            {
+
+                //let's just switch the data instead of the actual
+                //nodes and save a big headache here
+                helper->arrivalTime=root->arrivalTime;
+                helper->burstTime= root->burstTime;
+                strcpy(helper->processNumber, root->processNumber);
+
+
+                root->arrivalTime=root->next->arrivalTime;
+                root->burstTime=root->next->burstTime;
+                strcpy(root->processNumber,root->next->processNumber);
+
+                root->next->arrivalTime=helper->arrivalTime;
+                root->next->burstTime=helper->burstTime;
+                strcpy(root->next->processNumber,helper->processNumber);
+
+                switchFlag=1;
+            }
+
+            root=root->next;
+        }
+
+
+    }
+
+}
+
+
+
 
 
 
